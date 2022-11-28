1cbcaba5c848453fb48a71f6f0fd3ed44484f7fd
ArvinthS18 <your_email_address@example.com>
2022-11-28 17:59:36 +0530

    Added new user option

--- backend/base.py
+++ backend/base.py
@@ -10,40 +10,48 @@ app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/flask12"
 app.config["DEBUG"] = True
 mongo = PyMongo(app)
 names = mongo.db.example
+filter_status = 'false'
 # name12=''
 
 @app.route('/', methods=['GET'])
 def get():
-    arg1=request.args.get("sort")
-    arg2=request.args.get("filter")
-    # name12=arg2
+    arg1 = (request.args.get('filter'))
     if len(request.args.keys()) == 0:
         return api_all()
     elif 'filter' in request.args.keys():
-       return filter(str(arg2))
-    elif arg1=="sortbyasc":
+       return filter(str(arg1))
+    elif request.args.get('sort') == "sortbyasc":
         return sort_Asc()
-    elif arg1=="sortbydsc":
+    elif request.args.get('sort') == "sortbydsc":
         return sort_Dsc()
     # return find(name12)
     
 def api_all():
-    usersdb = mongo.db.example.find()
-    y = dumps(usersdb)
-    print(y) 
+    global filter_status
+    x = names.find()
+    y = dumps(x) 
     authors = json.loads(y)
-    return authors
+    filter_status = 'false'
+    return authors 
+    
 def sort_Asc():
-    x = names.find().sort("Name",1)
-    y = dumps(x) 
-    authors = json.loads(y)
-    return authors
+    if filter_status == 'true':
+        return (sorted(filterd_1, key=lambda i: (i['Name']),reverse=False))
+    elif filter_status == 'false':
+        x = names.find().sort("Name",1)
+        y = dumps(x) 
+        authors = json.loads(y)
+        return authors
+
 def sort_Dsc():
-    x = names.find().sort("Name",-1)
-    y = dumps(x) 
-    authors = json.loads(y)
-    return authors
-# app.add_url_rule("/sortbyasc","sort_Asc",sort_Asc)
+    if filter_status == 'true':
+        return (sorted(filterd_1, key=lambda i: (i['Name']),reverse=True))
+    elif filter_status == 'false':
+        x = names.find().sort("Name",-1)
+        y = dumps(x) 
+        authors = json.loads(y)
+        return authors
+# # app.add_url_rule("/sortbyasc","sort_Asc",sort_Asc)
 
 def find(name):
     print(name)
@@ -60,6 +68,7 @@ def find(name):
 app.add_url_rule("/<string:name>","find",find)
 
 def filter(name):
+    global filterd_1,filter_status
     x = names.find()
     y = dumps(list(x), indent = 2) 
     authors = json.loads(y)
@@ -69,6 +78,7 @@ def filter(name):
             z.append(x)
     a = dumps(z, indent = 2) 
     b = json.loads(a)
+    filterd_1,filter_status = b, 'true'
     if request.args.get('sort') == 'sortbyasc':
       return (sorted(b, key=lambda i: (i['Name']),reverse=False))
     elif request.args.get('sort') == 'sortbydsc':
@@ -96,18 +106,31 @@ def filter(name):
 #     return b
 
 
+# @app.route("/",methods=['GET'])
+# def get_details():    
+#     arg1 = (request.args.get('filter'))
+#     if len(request.args.keys()) == 0:
+#         return home_page()
+#     elif 'filter' in request.args.keys():
+#        return filter(str(arg1))
+#     elif request.args.get('sort') == "sortbyasc":
+#         return sort_Asc()
+#     elif request.args.get('sort') == "sortbydsc":
+#         return sort_Dsc()
+        
+
 @app.route('/api/adduser', methods=['POST'])
 def api_id():
     req = request.get_json()   
     NAME = req['Name']
     CLASS = req['class']
-    ROW1 = req['Roll no']
+    ROW1 = req['Rollno']
     MARK2 = req['Mark']
     # mongo.db.example.insert_one({"Name" : NAME, "class" : CLASS, "Roll no" : ROW1,"Mark" : MARK2})
     # return "New data added"  
     names.create_index("Name",unique=True)
     try :
-      names.insert_one({"Name" : NAME, "class" : CLASS, "Roll no" : ROW1,"Mark" : MARK2})
+      names.insert_one({"Name" : NAME, "class" : CLASS, "Rollno" : ROW1,"Mark" : MARK2})
     except :
         return "Name already exist" 
     return "New data added" 
--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import axios from "axios";
-
+import $ from "jquery";
+// import Button from "react-bootstrap/Button";
+// import Modal from "react-bootstrap/Modal";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,10 +14,17 @@ class App extends Component {
       Lib2: [],
       Sort: "asc",
       De_sort: "dsc",
+      show: false,
     };
     this.api_call(this.state.Lib);
     this.sorting_item = this.sorting_item.bind(this);
   }
+  // show123() {
+  //   this.setState({ show: true });
+  // }
+  // handleShow() {
+  //   this.setState({ show: false });
+  // }
 
   api_call(lib1) {
     axios({
@@ -38,16 +47,34 @@ class App extends Component {
     }
   }
   filtered_item(item, e) {
-    if (this.state.Sort == "asc") {
-      this.api_call(
-        "http://127.0.0.1:5000/?sort=sortbyasc" + "&filter=" + item
-      );
+    if (item.length > 0) {
+      if (this.state.Sort == "asc") {
+        this.api_call(
+          "http://127.0.0.1:5000/?sort=sortbyasc" + "&filter=" + item
+        );
+      }
+      if (this.state.Sort == "dsc") {
+        this.api_call(
+          "http://127.0.0.1:5000/?sort=sortbydsc" + "&filter=" + item
+        );
+      }
     }
-    if (this.state.Sort == "dsc") {
-      this.api_call(
-        "http://127.0.0.1:5000/?sort=sortbydsc" + "&filter=" + item
-      );
-    }
+  }
+
+  add_new_ele(e) {
+    $.ajax({
+      type: "POST",
+      url: "http://127.0.0.1:5000/api/adduser",
+      contentType: "application/json",
+      data: JSON.stringify({
+        Mark: document.getElementById("form4Example2").value,
+        Name: document.getElementById("form4Example4").value,
+        Rollno: document.getElementById("form4Example6").value,
+        class: document.getElementById("form4Example8").value,
+      }),
+      dataType: "json",
+    });
+    window.location.reload();
   }
 
   render() {
@@ -74,9 +101,16 @@ class App extends Component {
             Search
           </button>
         </center>
-        <button type="button" class="btn btn-dark">
-          Add user
+        <button
+          type="button"
+          class="btn btn-lg btn-link"
+          style={{ marginLeft: "900px" }}
+          data-mdb-toggle="modal"
+          data-mdb-target="#exampleModal"
+        >
+          Add Students
         </button>
+
         <div className="container">
           <table
             className="table table-striped table-dark"
@@ -85,6 +119,7 @@ class App extends Component {
             <thead>
               <tr>
                 <td>S.NO</td>
+                <td>Roll NO</td>
                 <td>
                   Name
                   <i
@@ -100,7 +135,7 @@ class App extends Component {
               {this.state.Lib2.map((x, index) => (
                 <tr key={index}>
                   <td>{index + 1}</td>
-
+                  <td>{x.Rollno}</td>
                   <td>{x.Name}</td>
                   <td>{x.Mark}</td>
                   <td>{x.class}</td>
@@ -108,6 +143,124 @@ class App extends Component {
               ))}
             </tbody>
           </table>
+        </div>
+        {/* <Button
+          variant="primary"
+          onClick={() => {
+            this.show123();
+          }}
+        >
+          Launch demo modal
+        </Button>
+
+        <Modal show={this.state.show}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary">Close</Button>
+            <Button variant="primary">Save Changes</Button>
+          </Modal.Footer>
+        </Modal> */}
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Add New Student Details
+                </h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-mdb-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <div className="card-body">
+                  <div className="container d-flex ">
+                    <form>
+                      <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">
+                          Mark{" "}
+                        </span>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="form4Example2"
+                          aria-label="Username"
+                          aria-describedby="addon-wrapping"
+                        />
+                      </div>
+                      <br />
+                      <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">
+                          Name{" "}
+                        </span>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="form4Example4"
+                          aria-label="Username"
+                          aria-describedby="addon-wrapping"
+                        />
+                      </div>
+                      <br />
+                      <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">
+                          Rollno{" "}
+                        </span>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="form4Example6"
+                          aria-label="Username"
+                          aria-describedby="addon-wrapping"
+                        />
+                      </div>
+                      <br />
+                      <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">
+                          class{" "}
+                        </span>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="form4Example8"
+                          aria-label="Username"
+                          aria-describedby="addon-wrapping"
+                        />
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-mdb-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  data-mdb-dismiss="modal"
+                  onClick={(e) => this.add_new_ele(e)}
+                  class="btn btn-primary"
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
